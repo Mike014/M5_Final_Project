96f4d8dba527e0d716d567811535517a1cc5700c
Mike014 <mikgrimaldi7@gmail.com>
2026-02-20 08:58:17 +0100

    Initial Commit

---
+++ Assets/Script/Gameplay/Player/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    // Referenza all'agent
+    private NavMeshAgent _agent;
+
+    // La camera da cui partiamo per il Raycast
+    // (deve puntare alla camera principale)
+    private Camera _camera;
+
+    // Utilizzo Awake piuttosto che Start in modo tale che i riferimenti siano stati già trovati
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        // Input.GetMouseButtonDown(0) = click sinistro
+        // "Down" = solo il frame in cui viene premuto (non held)
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToClickPosition();
+        }
+    }
+
+    private void MoveToClickPosition()
+    {
+        // ScreenPointToRay converte la posizione 2D del mouse
+        // in un raggio 3D che parte dalla camera
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        // RaycastHit contiene tutte le info sul punto colpito
+        RaycastHit hit;
+
+        // Physics.Raycast lancia il raggio nella scena
+        // "out hit" popola la struct con i dati del collider colpito
+        if (Physics.Raycast(ray, out hit))
+        {
+            // SetDestination è il cuore: passiamo il punto 3D
+            // all'agent che calcola il percorso automaticamente
+            _agent.SetDestination(hit.point);
+        }
+    }
+}
